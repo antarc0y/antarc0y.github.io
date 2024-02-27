7023d90fe6062017a51eaff84e8346b001163a80
yuihan <98428883+antarc0y@users.noreply.github.com>
2024-02-27 13:51:49 -0700

    added background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,22 @@
 <script>
 	import Header from './Header.svelte';
+	import Background from '../components/background.svelte';
 	import './styles.css';
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+<div class="background">
+	<Background />
 </div>
 
+<slot />
+
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
+	.background {
+		position: fixed;
+		top: 0;
+		left: 0;
 		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
+		height: 100%;
+		z-index: -1;
 	}
 </style>
