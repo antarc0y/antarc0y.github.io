9e39420f76908bd18891c277df094766dd708c22
yuihan <98428883+antarc0y@users.noreply.github.com>
2024-03-11 18:37:13 -0600

    added all icons and card hover effect to skill section

--- src/components/skills.svelte
+++ src/components/skills.svelte
@@ -8,149 +8,6 @@
 	<div class="experience-details-container">
 		<div class="about-containers">
 			<div class="details-container">
-				<h2 class="experience-sub-title">database management</h2>
-				<div class="article-container">
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/189716855-2c69ca7a-5149-4647-936d-780610911353.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>Firebase</h3>
-							<p>Basic</p>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>MongoDB</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/192107856-aa92c8b1-b615-47c3-9141-ed0d29a90239.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>GraphQL</h3>
-						</div>
-					</article>
-					<article>
-						<img src="src/icons/checkmark.png" alt="Experience icon" class="icon" />
-						<div>
-							<h3>SQL</h3>
-						</div>
-					</article>
-				</div>
-			</div>
-			<div class="details-container">
-				<h2 class="experience-sub-title">frontend development</h2>
-				<div class="article-container">
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/192158956-48192682-23d5-4bfc-9dfb-6511ade346bc.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>SASS</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://www.w3.org/html/logo/downloads/HTML5_Badge.svg"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>HTML</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>CSS</h3>
-						</div>
-					</article>
-
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>React JS / Native</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://raw.githubusercontent.com/sveltejs/branding/master/svelte-logo.svg"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>Svelte</h3>
-						</div>
-					</article>
-				</div>
-			</div>
-			<div class="details-container">
-				<h2 class="experience-sub-title">backend development</h2>
-				<div class="article-container">
-					<article>
-						<img
-							src="https://wiki.postgresql.org/images/a/a4/PostgreSQL_logo.3colors.svg"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>PostgreSQL</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>Node JS</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://www.svgrepo.com/show/353657/django-icon.svg"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>Django</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/183570228-6a040b9f-3ddf-47a2-a201-743121dac664.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>PHP</h3>
-						</div>
-					</article>
-				</div>
-			</div>
-			<div class="details-container">
 				<h2 class="experience-sub-title">languages</h2>
 				<div class="article-container">
 					<article>
@@ -223,6 +80,144 @@
 							<h3>C#</h3>
 						</div>
 					</article>
+
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/192149581-88194d20-1a37-4be8-8801-5dc0017ffbbe.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Go</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div class="details-container">
+				<h3 class="experience-sub-title">frontend development</h3>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/192158956-48192682-23d5-4bfc-9dfb-6511ade346bc.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Sass</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://www.w3.org/html/logo/downloads/HTML5_Badge.svg"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>HTML</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>CSS</h3>
+						</div>
+					</article>
+
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>React</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://raw.githubusercontent.com/sveltejs/branding/master/svelte-logo.svg"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Svelte</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+			<div class="details-container">
+				<h2 class="experience-sub-title">backend development</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Node JS</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://www.svgrepo.com/show/353657/django-icon.svg"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Django</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183570228-6a040b9f-3ddf-47a2-a201-743121dac664.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>PHP</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183423775-2276e25d-d43d-4e58-890b-edbc88e915f7.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Flask</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://github.com/marwin1991/profile-technology-icons/assets/62091613/9bf5650b-e534-4eae-8a26-8379d076f3b4"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Django REST</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+			<div class="details-container">
+				<h2 class="experience-sub-title">mobile</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/9/95/Android_Studio_Icon_3.6.svg"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Andriod Studio</h3>
+						</div>
+					</article>
 					<article>
 						<img
 							src="https://user-images.githubusercontent.com/25181517/186150365-da1eccce-6201-487c-8649-45e9e99435fd.png"
@@ -230,7 +225,7 @@
 							class="icon"
 						/>
 						<div>
-							<h3>DART</h3>
+							<h3>Dart</h3>
 						</div>
 					</article>
 					<article>
@@ -243,39 +238,20 @@
 							<h3>Flutter</h3>
 						</div>
 					</article>
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/192149581-88194d20-1a37-4be8-8801-5dc0017ffbbe.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>Go</h3>
-						</div>
-					</article>
-					<article>
-						<img
-							src="https://user-images.githubusercontent.com/25181517/187955005-f4ca6f1a-e727-497b-b81b-93fb9726268e.png"
-							alt="Experience icon"
-							class="icon"
-						/>
-						<div>
-							<h3>Jest</h3>
-						</div>
-					</article>
 				</div>
 			</div>
+
 			<div class="details-container">
-				<h2 class="experience-sub-title">development tools and environments</h2>
+				<h2 class="experience-sub-title">tools</h2>
 				<div class="article-container">
 					<article>
 						<img
-							src="https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png"
+							src="https://user-images.githubusercontent.com/25181517/183912952-83784e94-629d-4c34-a961-ae2ae795b662.png"
 							alt="Experience icon"
 							class="icon"
 						/>
 						<div>
-							<h3>Git</h3>
+							<h3>Jira</h3>
 						</div>
 					</article>
 					<article>
@@ -290,30 +266,169 @@
 					</article>
 					<article>
 						<img
-							src="https://user-images.githubusercontent.com/25181517/183912952-83784e94-629d-4c34-a961-ae2ae795b662.png"
+							src="https://user-images.githubusercontent.com/25181517/192109061-e138ca71-337c-4019-8d42-4792fdaa7128.png"
 							alt="Experience icon"
 							class="icon"
 						/>
 						<div>
-							<h3>Jira</h3>
-						</div>
-					</article>
-					<article>
-						<img src="src/icons/checkmark.png" alt="Experience icon" class="icon" />
-						<div>
-							<h3>Visual Studio</h3>
+							<h3>Postman</h3>
 						</div>
 					</article>
 					<article>
 						<img
-							src="https://upload.wikimedia.org/wikipedia/commons/9/95/Android_Studio_Icon_3.6.svg"
+							src="https://user-images.githubusercontent.com/25181517/121401671-49102800-c959-11eb-9f6f-74d49a5e1774.png"
 							alt="Experience icon"
 							class="icon"
 						/>
 						<div>
-							<h3>Andriod Studio</h3>
+							<h3>npm</h3>
 						</div>
 					</article>
+				</div>
+			</div>
+
+			<div class="details-container">
+				<h2 class="experience-sub-title">database management</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/189716855-2c69ca7a-5149-4647-936d-780610911353.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Firebase</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>MongoDB</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/192107856-aa92c8b1-b615-47c3-9141-ed0d29a90239.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>GraphQL</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://wiki.postgresql.org/images/a/a4/PostgreSQL_logo.3colors.svg"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>PostgreSQL</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183896128-ec99105a-ec1a-4d85-b08b-1aa1620b2046.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>MySQL</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div class="details-container">
+				<h2 class="experience-sub-title">Cloud</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183911547-990692bc-8411-4878-99a0-43506cdb69cf.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Google Cloud</h3>
+						</div>
+					</article>
+					<article>
+						<img src="src/icons/heroku.svg" alt="heroku icon" class="icon" />
+						<div>
+							<h3>Heroku</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div class="details-container">
+				<h2 class="experience-sub-title">testing</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/187955005-f4ca6f1a-e727-497b-b81b-93fb9726268e.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Jest</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/184103699-d1b83c07-2d83-4d99-9a1e-83bd89e08117.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Selenium</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div class="details-container">
+				<h2 class="experience-sub-title">UI/UX</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Figma</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/189716630-fe6c084c-6c66-43af-aa49-64c8aea4a5c2.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Material UI</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/02494c7c-de6a-43a6-9293-6369696842ed"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Canva</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+			<div class="details-container">
+				<h2 class="experience-sub-title">game</h2>
+				<div class="article-container">
 					<article>
 						<img
 							src="https://user-images.githubusercontent.com/25181517/193427941-9437dbbe-376f-40dc-9573-0ef5c02a26a7.png"
@@ -322,6 +437,49 @@
 						/>
 						<div>
 							<h3>Unity</h3>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div class="details-container">
+				<h2 class="experience-sub-title">DevOps</h2>
+				<div class="article-container">
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Git</h3>
+						</div>
+					</article>
+					<article>
+						<img src="src/icons/github.png" alt="Experience icon" class="icon" />
+						<div>
+							<h3>Github</h3>
+						</div>
+					</article>
+
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/192158606-7c2ef6bd-6e04-47cf-b5bc-da2797cb5bda.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>bash</h3>
+						</div>
+					</article>
+					<article>
+						<img
+							src="https://user-images.githubusercontent.com/25181517/183345125-9a7cd2e6-6ad6-436f-8490-44c903bef84c.png"
+							alt="Experience icon"
+							class="icon"
+						/>
+						<div>
+							<h3>Nginx</h3>
 						</div>
 					</article>
 				</div>
