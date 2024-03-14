f36804e58c3d90cab24e857171ade4e9f5ef7d5a
yuihan <98428883+antarc0y@users.noreply.github.com>
2024-03-14 10:32:21 -0600

    supress prerender error

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,14 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			handleHttpError({ status, path, referrer, referenceType }) {
+				if (status === 404) {
+					console.warn(`Suppressing 404 error for ${path}`);
+					return true; // Suppress the error
+				}
+			}
+		}
 	}
 };
 
