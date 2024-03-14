fccd15be01683bf54fa349765d93a497482c8bea
yuihan <98428883+antarc0y@users.noreply.github.com>
2024-03-14 11:00:08 -0600

    updated config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			handleHttpError({ status, path, referrer, referenceType }) {
-				if (status === 404) {
-					console.warn(`Suppressing 404 error for ${path}`);
-					return true; // Suppress the error
-				}
+		adapter: adapter(
+
+			{
+				pages: 'build',
+				assets: 'build',
+				fallback: null
 			}
-		}
+		), appDir: 'app'
+
 	}
 };
 
