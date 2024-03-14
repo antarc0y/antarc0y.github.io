165cf03b87cb45c01409825aa7fc36cea78825f2
yuihan <98428883+antarc0y@users.noreply.github.com>
2024-03-14 11:50:15 -0600

    using adapter static

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+
 };
 
 export default config;
